458e3f0497e6fa356ee6cd8d4c6be8fa91f2dac2
David Okun <david@okun.io>
2020-07-05 15:57:58 -0500

    Better logging, including password

--- decrypt-secrets.sh
+++ decrypt-secrets.sh
@@ -6,37 +6,38 @@ IOS_PROJECT_KEYS=${1}
 
 echo "========================================"
 echo "Decrypting certs and profiles..."
-echo "========================================"
 
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROJECT_KEYS" --output ./.github/secrets/Afterparty_2020_Ad_Hoc_Profile.mobileprovision ./.github/secrets/Afterparty_2020_Ad_Hoc_Profile.mobileprovision.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROJECT_KEYS" --output ./.github/secrets/AfterpartyAdHocDistributionCert.p12 ./.github/secrets/AfterpartyAdHocDistributionCert.p12.gpg
 
 echo "========================================"
-echo "Making Provisioning Profiles directory"
-echo "========================================"
+echo "Making Provisioning Profiles directory..."
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
 echo "========================================"
 echo "Copying provisioning profile..."
-echo "========================================"
 
 cp ./.github/secrets/Afterparty_2020_Ad_Hoc_Profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/Afterparty_2020_Ad_Hoc_Profile.mobileprovision
 
 echo "========================================"
 echo "Creating keychain..."
-echo "========================================"
 
 security create-keychain -p "" build.keychain
 
 echo "========================================"
-echo "Importing certs into keychain"
-echo "========================================"
+echo "Importing certs into keychain..."
 
-security import ./.github/secrets/AfterpartyAdHocDistributionCert.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+security import ./.github/secrets/AfterpartyAdHocDistributionCert.p12 -t agg -k ~/Library/Keychains/build.keychain -P $IOS_PROJECT_KEYS -A
+
+echo "========================================"
+echo "Unlocking main keychain..."
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
 security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+
+echo "========================================"
+echo "Setting key partition..."
 
 security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
