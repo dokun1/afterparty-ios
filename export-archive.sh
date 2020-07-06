56e105b46d829c30388b435d786db06ae494769e
David Okun <david@okun.io>
2020-07-06 18:18:05 -0500

    Ok, does the export command and the node install command work now?

--- export-archive.sh
+++ export-archive.sh
@@ -2,7 +2,6 @@
 
 set -eo pipefail
 
-xcodebuild -exportArchive \ 
-           -archivePath ./Afterparty.xcarchive \
+xcodebuild -exportArchive -archivePath ./Afterparty.xcarchive \
            -exportOptionsPlist ./ExportOptions.plist \
            -exportPath ./AfterpartyProducts | xcpretty
