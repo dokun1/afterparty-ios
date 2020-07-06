3343d7009d842ca4bcf20dd5bd24689c5527188a
David Okun <david@okun.io>
2020-07-06 18:10:33 -0500

    oh my god the archive worked

--- export-archive.sh
+++ export-archive.sh
@@ -3,10 +3,6 @@
 set -eo pipefail
 
 xcodebuild -exportArchive \ 
-           DEVELOPMENT_TEAM=RQ632LL2X3 \
-           CODE_SIGN_STYLE=Manual \
-           CODE_SIGN_IDENTITY="Apple Distribution: David Okun" \
-           PROVISION_PROFILE="AfterpartyAdHoc.mobileprovision" \
            -archivePath ./Afterparty.xcarchive \
            -exportOptionsPlist ./ExportOptions.plist \
            -exportPath ./AfterpartyProducts | xcpretty
