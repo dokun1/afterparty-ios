04237240d74609e94dbe0476afa3eb4aec85fca7
David Okun <david@okun.io>
2020-08-02 16:04:19 -0500

    Poking at straws to fix the scheme

--- test-app-staging.sh
+++ test-app-staging.sh
@@ -5,5 +5,5 @@ set -eo pipefail
 xcodebuild -project Afterparty.xcodeproj \
            -scheme "Afterparty Staging Tests" \
            -sdk iphonesimulator \
-           -destination 'platform=iOS Simulator,name=iPhone 11 Pro Max' \
+           -destination 'platform=iOS Simulator,name=iPhone 11 Pro Max,OS=latest' \
             clean build-for-testing test
