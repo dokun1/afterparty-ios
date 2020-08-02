da7463504183a3d7b97c801629c53d86e87bc089
David Okun <david@okun.io>
2020-08-02 16:18:30 -0500

    Running out of ideas

--- test-app-staging.sh
+++ test-app-staging.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-set -eo pipefail
+# set -eo pipefail
 
 xcodebuild -project Afterparty.xcodeproj \
            -scheme "Afterparty Staging Tests" \
            -sdk iphonesimulator \
            -destination 'platform=iOS Simulator,name=iPhone 11 Pro Max,OS=latest' \
-            clean build-for-testing test
+           -verbose \
+            test
