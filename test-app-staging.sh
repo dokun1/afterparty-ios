44711de41b6d6f957e3f3cf8cbd0fff99ad9e246
David Okun <david@okun.io>
2020-08-02 15:49:47 -0500

    Maybe removing the OS version will help

--- test-app-staging.sh
+++ test-app-staging.sh
@@ -5,5 +5,5 @@ set -eo pipefail
 xcodebuild -project Afterparty.xcodeproj \
            -scheme "Afterparty Staging Tests" \
            -sdk iphonesimulator \
-           -destination 'platform=iOS Simulator,name=iPhone 11,OS=13.6' \
+           -destination 'platform=iOS Simulator,name=iPhone 11 Pro Max' \
             clean build-for-testing test
