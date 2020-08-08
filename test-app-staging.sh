d28869b6350f705bf12247651ba561e28a8210f4
David Okun <david@okun.io>
2020-08-08 13:30:16 -0500

    Cookie monster how bout now

--- test-app-staging.sh
+++ test-app-staging.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 sudo xcode-select -switch /Applications/Xcode_11.6.app
 
 xcodebuild -project Afterparty.xcodeproj \
-           -scheme "Afterparty Staging Tests" \
+	   -scheme "Afterparty Staging Tests" \
            -sdk iphonesimulator \
            -destination 'platform=iOS Simulator,name=iPhone 11 Pro Max,OS=latest' \
-            clean test
+            test
