aa8deeca274bee2752ce8de2bad04b836a2f43c8
David Okun <david@okun.io>
2020-08-08 13:20:22 -0500

    Come on now

--- test-app-staging.sh
+++ test-app-staging.sh
@@ -5,8 +5,7 @@ set -eo pipefail
 sudo xcode-select -switch /Applications/Xcode_11.6.app
 
 xcodebuild -project Afterparty.xcodeproj \
-           -scheme "Afterparty Staging" \
+           -scheme "Afterparty Staging Tests" \
            -sdk iphonesimulator \
            -destination 'platform=iOS Simulator,name=iPhone 11 Pro Max,OS=latest' \
-           -verbose \
-            test
+            clean test
