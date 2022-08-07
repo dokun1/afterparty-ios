daf595502c9356b97d6a26d5366feb657f569061
David Okun <david@okun.io>
2022-08-07 16:39:28 -0500

    Updating staging pipeline secret injection script

--- inject-staging.sh
+++ inject-staging.sh
@@ -6,6 +6,8 @@ rootURLHost=${3}
 rootURLScheme=${4}
 appName=${5}
 appBundleID=${6}
+foursquareAPIKey=${7}
+parseApplicationID=${8}
 
 echo '
 MS_APP_CENTER_SECRET = '$appCenterSecret'
@@ -15,4 +17,6 @@ ROOT_URL_SCHEME = '$rootURLScheme'
 
 APP_NAME = '$appName'
 APP_BUNDLE_ID = '$appBundleID'
+FOURSQUARE_API_KEY = '$foursquareAPIKey'
+PARSE_APPLICATION_ID = '$parseApplicationID'
 ' > ./Afterparty/Config/Environment/Staging.xcconfig
